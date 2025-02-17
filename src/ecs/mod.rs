37b93cf0dfb5a921778c97ef1b822d5238b56aab
Scott Munday <scottmunday84@gmail.com>
2025-02-17 16:55:45 -0500

    Added macros to preamble

--- src/ecs/mod.rs
+++ src/ecs/mod.rs
@@ -2,6 +2,7 @@ pub mod component;
 pub mod system;
 
 pub mod preamble {
+    pub use crate::{get_component, has_component};
     pub use crate::ecs::component::{ComponentCheck, Components, IComponent};
     pub use crate::ecs::system::System;
 }
