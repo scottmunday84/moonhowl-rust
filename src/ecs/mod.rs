05736e5716fde2c6d42de168657431a2b58dc181
Scott Munday <scottmunday84@gmail.com>
2025-02-17 16:57:44 -0500

    preamble becomes prelude

--- src/ecs/mod.rs
+++ src/ecs/mod.rs
@@ -1,7 +1,7 @@
 pub mod component;
 pub mod system;
 
-pub mod preamble {
+pub mod prelude {
     pub use crate::{get_component, has_component};
     pub use crate::ecs::component::{ComponentCheck, Components, IComponent};
     pub use crate::ecs::system::System;
