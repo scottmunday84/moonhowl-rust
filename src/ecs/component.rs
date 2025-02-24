09eb64c6cd26c1ae70dd7e5951fdfac4dd84214c
Scott Munday <scottmunday84@gmail.com>
2025-02-24 15:29:42 -0500

    Added macro for setting up a struct to use a proper impl

--- src/ecs/component.rs
+++ src/ecs/component.rs
@@ -4,12 +4,6 @@ pub trait IComponent: Any {
     fn as_any(&self) -> &dyn Any;
 }
 
-impl<T: Any> IComponent for T {
-    fn as_any(&self) -> &dyn Any {
-        self
-    }
-}
-
 #[macro_export]
 macro_rules! ecs_component {
     ($t:ty) => {
