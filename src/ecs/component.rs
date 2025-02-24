b664c373b12d60334eeab9ee56356f6117d36c5d
Scott Munday <scottmunday84@gmail.com>
2025-02-24 15:18:11 -0500

    Added macro for setting up a struct to use a proper impl

--- src/ecs/component.rs
+++ src/ecs/component.rs
@@ -9,3 +9,14 @@ impl<T: Any> IComponent for T {
         self
     }
 }
+
+#[macro_export]
+macro_rules! ecs_component {
+    ($t:ty) => {
+        impl IComponent for $t {
+            fn as_any(&self) -> &dyn Any {
+                self
+            }
+        }
+    };
+}
--- src/ecs/mod.rs
+++ src/ecs/mod.rs
@@ -3,6 +3,7 @@ mod entity;
 pub mod system;
 
 pub mod prelude {
+    pub use crate::ecs_component;
     pub use crate::ecs::component::IComponent;
     pub use crate::ecs::entity::{Entity, EntityCheck, EntitySystem};
     pub use crate::ecs::system::System;
