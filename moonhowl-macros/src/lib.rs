024d61db884a7818d63adb59009fba484f8f9730
Scott Munday <scottmunday84@gmail.com>
2025-12-25 17:45:39 -0500

    Added some restructure to take advantage of attributes

---
+++ moonhowl-macros/src/lib.rs
@@ -0,0 +1,20 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, ItemStruct};
+
+#[proc_macro_attribute]
+pub fn ecs_component(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as ItemStruct);
+    let name = &input.ident;
+
+    quote! {
+        #input
+
+        impl IComponent for #name {
+            fn as_any(&self) -> &dyn std::any::Any {
+                self
+            }
+        }
+    }
+    .into()
+}
