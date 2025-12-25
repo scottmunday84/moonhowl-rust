70606c8ac9555889357d505e068a8913e40f3d62
Scott Munday <scottmunday84@gmail.com>
2025-12-25 18:23:13 -0500

    Moved workspace around so it's cleaner and can use the macro attributes

--- moonhowl-ecs/src/lib.rs
+++ moonhowl-ecs/src/lib.rs
@@ -1,1 +1,10 @@
-pub mod ecs;
+pub mod component;
+mod entity;
+pub mod system;
+
+pub mod prelude {
+    pub use crate::component::IComponent;
+    pub use crate::entity::{Entity, EntityCheck, EntitySystem};
+    pub use crate::system::System;
+    pub use crate::ecs_component;
+}
