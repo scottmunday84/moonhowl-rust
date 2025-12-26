f8aa1598e2e386e184a0a40a0986c5534b7b89ab
Scott Munday <scottmunday84@gmail.com>
2025-12-25 19:14:23 -0500

    Dropped macro instead for macro attribute

--- moonhowl-ecs/src/component.rs
+++ moonhowl-ecs/src/component.rs
@@ -3,14 +3,3 @@ use std::any::Any;
 pub trait IComponent: Any {
     fn as_any(&self) -> &dyn Any;
 }
-
-#[macro_export]
-macro_rules! ecs_component {
-    ($t:ty) => {
-        impl IComponent for $t {
-            fn as_any(&self) -> &dyn Any {
-                self
-            }
-        }
-    };
-}
