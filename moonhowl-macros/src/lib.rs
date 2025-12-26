04b7587b374446fe69f2f8b09a9b48710bfad04d
Scott Munday <scottmunday84@gmail.com>
2025-12-25 19:15:56 -0500

    Reduced moonhowl-macros (again)

--- moonhowl-macros/src/lib.rs
+++ moonhowl-macros/src/lib.rs
@@ -1,3 +1,20 @@
-mod ecs_macros;
+pub use proc_macro::TokenStream;
+pub use quote::quote;
+pub use syn::{parse_macro_input, ItemStruct};
 
-pub use ecs_macros::ecs_component;
+#[proc_macro_attribute]
+pub fn ecs_component(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as ItemStruct);
+    let name = &input.ident;
+
+    quote! {
+        #input
+
+        impl IComponent for #name {
+            fn as_any(&self) -> &dyn std::any::Any {
+                self
+            }
+        }
+    }
+        .into()
+}
